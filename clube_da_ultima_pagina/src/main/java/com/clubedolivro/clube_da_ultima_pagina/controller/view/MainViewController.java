fc86498ea8ba1845526b1b7c2f4e5d044207e65d
Kevin Siqueira Perdomo <kevin.siqueira23@gmail.com>
2025-08-10 09:59:32 -0300

    organizando ...

--- clube_da_ultima_pagina/src/main/java/com/clubedolivro/clube_da_ultima_pagina/controller/view/MainViewController.java
+++ clube_da_ultima_pagina/src/main/java/com/clubedolivro/clube_da_ultima_pagina/controller/view/MainViewController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping(path = "principal")
+@RequestMapping(path = "helloworld")
 public class MainViewController {
     @GetMapping("/")
     public String getHomePage() {
-        return "index.html";
+        return "helloworld.html";
     }
     
 }
