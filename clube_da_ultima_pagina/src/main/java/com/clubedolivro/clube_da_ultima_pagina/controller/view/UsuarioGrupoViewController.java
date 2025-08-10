6e9f09818e049e5f341290202d6abaf80acae7a0
Kevin Siqueira Perdomo <kevin.siqueira23@gmail.com>
2025-08-10 11:49:58 -0300

    Adiciona controlador e template para a página de ação administrativa

--- clube_da_ultima_pagina/src/main/java/com/clubedolivro/clube_da_ultima_pagina/controller/view/UsuarioGrupoViewController.java
+++ clube_da_ultima_pagina/src/main/java/com/clubedolivro/clube_da_ultima_pagina/controller/view/UsuarioGrupoViewController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UsuariogrupoViewController {
+public class UsuarioGrupoViewController {
     
     @GetMapping("/usuario_grupo")
     public String usuarioGrupoPage() {
