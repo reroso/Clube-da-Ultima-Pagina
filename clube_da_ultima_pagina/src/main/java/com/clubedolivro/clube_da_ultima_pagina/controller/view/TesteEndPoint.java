355041768a44044d390d0f1b5d255c489d72e716
Kevin Siqueira Perdomo <kevin.siqueira23@gmail.com>
2025-08-10 10:16:37 -0300

    Testando ...

---
+++ clube_da_ultima_pagina/src/main/java/com/clubedolivro/clube_da_ultima_pagina/controller/view/TesteEndPoint.java
@@ -0,0 +1,18 @@
+package com.clubedolivro.clube_da_ultima_pagina.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequestMapping(path = "teste_endpoint")
+public class TesteEndPoint {
+    @GetMapping("/{id}")
+    public String getHomePage(@PathVariable("id") String id, Model model) {
+        model.addAttribute("id", id);
+        model.addAttribute("nomeGrupo", "Terror Futebol Clube (Esse atributo vem do controller)");
+        return "teste_endpoint";
+    }
+}
