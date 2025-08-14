2c7a7ee0d3419a073e2b10123e174a3bcd5aa0db
Kevin Siqueira Perdomo <kevinperdomo@campos.rj.gov.br>
2025-08-14 15:15:07 -0300

    Adiciona controlador de usuários com métodos para listar e cadastrar usuários

--- clube_da_ultima_pagina/src/main/java/com/clubedolivro/clube_da_ultima_pagina/controller/view/UsuarioViewController.java
+++ clube_da_ultima_pagina/src/main/java/com/clubedolivro/clube_da_ultima_pagina/controller/view/UsuarioViewController.java
@@ -1,13 +1,35 @@
 package com.clubedolivro.clube_da_ultima_pagina.controller.view;
 
+
+import com.clubedolivro.clube_da_ultima_pagina.entity.Usuario;
+import com.clubedolivro.clube_da_ultima_pagina.entity.PerfilEnum;
+import com.clubedolivro.clube_da_ultima_pagina.service.UsuarioService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UsuarioViewController {
 
+    private final UsuarioService usuarioService;
+
+    public UsuarioViewController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     @GetMapping("/usuario")
-    public String usuarioPage() {
-        return "usuario.html";
+    public String usuarioPage(Model model) {
+        model.addAttribute("usuarios", usuarioService.listarTodos());
+        model.addAttribute("usuarioForm", new Usuario());
+        model.addAttribute("tiposPerfil", PerfilEnum.values());
+        return "usuario";
+    }
+
+    @PostMapping("/usuario")
+    public String cadastrarUsuario(@ModelAttribute("usuarioForm") Usuario usuario) {
+        usuarioService.salvar(usuario);
+        return "redirect:/usuario";
     }
 }
